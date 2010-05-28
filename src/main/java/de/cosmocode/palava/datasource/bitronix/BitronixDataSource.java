b820e4f496916031d649f2586e217c41d2711dad
sarnowski <sarnowski@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-05-28 10:43:33 +0000

    use binding utility

--- src/main/java/de/cosmocode/palava/datasource/bitronix/BitronixDataSource.java
+++ src/main/java/de/cosmocode/palava/datasource/bitronix/BitronixDataSource.java
@@ -19,17 +19,16 @@ package de.cosmocode.palava.datasource.bitronix;
 import bitronix.tm.resource.jdbc.PoolingDataSource;
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.name.Named;
 import de.cosmocode.palava.core.lifecycle.Disposable;
 import de.cosmocode.palava.core.lifecycle.Initializable;
 import de.cosmocode.palava.core.lifecycle.LifecycleException;
 import de.cosmocode.palava.datasource.DataSourceConfig;
 import de.cosmocode.palava.datasource.DataSourceProvider;
+import de.cosmocode.palava.jndi.JndiContextBinderUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -40,31 +39,31 @@ import java.util.Properties;
  * @author Tobias Sarnowski
  */
 final class BitronixDataSource implements Initializable, Disposable, DataSourceProvider {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(BitronixDataSource.class);
 
-    private final Provider<Context> contextProvider;
+    private JndiContextBinderUtility jndiContextBinderUtility;
 
     private final String unique;
     private final String jndiName;
     private final String driver;
     private final Properties properties;
     private final int poolMax;
-    private final int poolMin;
 
+    private final int poolMin;
     private final PoolingDataSource dataSource = new PoolingDataSource();
 
     @Inject
     public BitronixDataSource(
-        Provider<Context> contextProvider,
+        JndiContextBinderUtility jndiContextBinderUtility,
         @Named(DataSourceConfig.UNIQUE) String unique,
         @Named(DataSourceConfig.JNDI_NAME) String jndiName,
         @Named(DataSourceConfig.DRIVER) String driver,
         @Named(DataSourceConfig.PROPERTIES) Properties properties,
         @Named(DataSourceConfig.POOL_MAX) int poolMax,
         @Named(DataSourceConfig.POOL_MIN) int poolMin) {
-        
-        this.contextProvider = Preconditions.checkNotNull(contextProvider, "ContextProvier");
+
+        this.jndiContextBinderUtility = jndiContextBinderUtility;
         this.unique = Preconditions.checkNotNull(unique, "Unique");
         this.jndiName = Preconditions.checkNotNull(jndiName, "JndiName");
         this.driver = Preconditions.checkNotNull(driver, "Driver");
@@ -75,8 +74,6 @@ final class BitronixDataSource implements Initializable, Disposable, DataSourceP
 
     @Override
     public void initialize() throws LifecycleException {
-        final Context context = contextProvider.get();
-
         dataSource.setUniqueName(unique);
         dataSource.setClassName(driver);
         dataSource.setMaxPoolSize(poolMax);
@@ -88,7 +85,7 @@ final class BitronixDataSource implements Initializable, Disposable, DataSourceP
             LOG.info("Binding DataSource {} to {} [{}]", new Object[] {
                 unique, jndiName, dataSource
             });
-            context.bind(jndiName, dataSource);
+            jndiContextBinderUtility.bind(jndiName, dataSource);
         } catch (NamingException e) {
             throw new LifecycleException(e);
         }
